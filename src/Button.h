747dfebe58d7edc80addf444231cfccaa6fe4214
Laszlo Hegedüs <laszlo.hegedues@gmail.com>
2023-05-02 01:01:05 +0200

    Add support for active state and only react to active multi button events

--- src/Button.h
+++ src/Button.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <Arduino.h>
+#include "settings.h"
+
 struct t_button {
 	const uint8_t gpio;
 	const uint8_t cmdShort;
 	const uint8_t cmdLong;
 	const bool enabled : 1;
+	const bool inverted : 1;
 	const bool internal : 1;
 	const bool wakeButton : 1;
 
@@ -16,9 +20,9 @@ struct t_button {
 	unsigned long lastReleasedTimestamp{0};
 	unsigned long firstPressedTimestamp{0};
 
-	t_button(uint8_t gpio, uint8_t cmdShort, uint8_t cmdLong) 
+	t_button(uint8_t gpio, uint8_t activeState, uint8_t cmdShort, uint8_t cmdLong) 
 		: gpio(gpio), cmdShort(cmdShort), cmdLong(cmdLong),
-		  enabled(gpio != 99), internal(gpio < GPIO_NUM_MAX), 
+		  enabled(gpio != 99), inverted(activeState == 1), internal(gpio < GPIO_NUM_MAX), 
 		  wakeButton(gpio == WAKEUP_BUTTON) {
 		lastState = currentState = isPressed = isReleased = false;
 	}
