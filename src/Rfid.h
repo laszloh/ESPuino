16f009b90c82fce6cb1b44a46ed5c7cf94d1eb75
Laszlo Hegedüs <laszlo.hegedues@gmail.com>
2023-11-19 16:03:16 +0100

    Do not silently disable settings
    In case of incompatible settings, we should inform the user instead of silenty disable one of them.

--- src/Rfid.h
+++ src/Rfid.h
@@ -8,7 +8,8 @@ void exit();
 void taskPause();
 void taskResume();
 void wakeupCheck();
-void preferenceLookupHandler();
+
+void resetOldRfid();
 
 } // namespace rfid
 
@@ -17,13 +18,7 @@ constexpr uint8_t cardIdStringSize = (4 * 3u) + 1u;
 
 extern char gCurrentRfidTagId[cardIdStringSize];
 
-#ifndef PAUSE_WHEN_RFID_REMOVED
-	#ifdef DONT_ACCEPT_SAME_RFID_TWICE // ignore feature silently if PAUSE_WHEN_RFID_REMOVED is active
-		#define DONT_ACCEPT_SAME_RFID_TWICE_ENABLE
-	#endif
-#endif
-
-#ifdef DONT_ACCEPT_SAME_RFID_TWICE_ENABLE
-void Rfid_ResetOldRfid(void);
-#endif
+// #ifdef DONT_ACCEPT_SAME_RFID_TWICE_ENABLE
+// void Rfid_ResetOldRfid(void);
+// #endif
 
