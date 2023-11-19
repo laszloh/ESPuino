a87f5b1440a066c4ea52980dad03c915fea72097
Laszlo Hegedüs <laszlo.hegedues@gmail.com>
2023-11-19 17:18:18 +0100

    use CardIdType instead of str's

--- src/Web.cpp
+++ src/Web.cpp
@@ -713,7 +713,7 @@ static void settingsToJSON(JsonObject obj, const String section) {
 		// current values
 		JsonObject curObj = obj.createNestedObject("current");
 		curObj["volume"].set(AudioPlayer_GetCurrentVolume());
-		curObj["rfidTagId"] = String(gCurrentRfidTagId);
+		curObj["rfidTagId"] = rfid::getCurrentRfidTagId().toDezimalString();
 	}
 	if ((section == "") || (section == "general")) {
 		// general settings
@@ -975,7 +975,7 @@ void Web_SendWebsocketData(uint32_t client, uint8_t code) {
 	} else if (code == 2) {
 		object["status"] = "error";
 	} else if (code == 10) {
-		object["rfidId"] = gCurrentRfidTagId;
+		object["rfidId"] = rfid::getCurrentRfidTagId().toDezimalString();
 	} else if (code == 20) {
 		object["pong"] = "pong";
 		object["rssi"] = Wlan_GetRssi();
