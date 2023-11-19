2979454a2678e5b6736bea7064ba5c2b0e40eb8d
Laszlo Hegedüs <laszlo.hegedues@gmail.com>
2023-11-19 17:17:39 +0100

    Use class internal function

--- src/RfidEvent.h
+++ src/RfidEvent.h
@@ -36,9 +36,9 @@ public:
 	}
 
 	const String toDezimalString() const {
-		char buf[3 * cardIdSize + 1] = {0};
+		char buf[3 * size() + 1] = {0};
 
-		for (int i = 0; i < cardIdSize; i++) {
+		for (int i = 0; i < size(); i++) {
 			const size_t idx = i * 3;
 			buf[idx] = '0' + operator[](i) / 100;
 			buf[idx + 1] = '0' + (operator[](i) % 100) / 10;
@@ -49,14 +49,14 @@ public:
 	}
 
 	const String toHexString() const {
-		char buf[3 * cardIdSize] = {0};
+		char buf[3 * size()] = {0};
 
 		constexpr char hexDigits[] = "0123456789ABCDEF";
-		for (size_t i = 0; i < cardIdSize; i++) {
+		for (size_t i = 0; i < size(); i++) {
 			const size_t idx = i * 3;
 			buf[idx] = hexDigits[(operator[](i) >> 4) & 0x0F];
 			buf[idx + 1] = hexDigits[operator[](i) & 0x0F];
-			buf[idx + 2] = (i < cardIdSize - 1) ? '-' : '\0';
+			buf[idx + 2] = (i < size() - 1) ? '-' : '\0';
 		}
 
 		return buf;
