6d665fbb5713241e0e79723e1887f6550f13bdfa
laszloh <laszlo.hegedues@gmail.com>
2023-11-27 17:17:10 +0100

    remove one cpp header

--- include/cpp.h
+++ include/cpp.h
@@ -3,10 +3,10 @@
 #include <array>
 #include <cstddef>
 
-namespace std {
-
 #ifndef __cpp_lib_to_array
 	#define __cpp_lib_to_array 201907L
+
+namespace std {
 
 namespace detail {
 
@@ -22,16 +22,21 @@ constexpr std::array<std::remove_cv_t<T>, N> to_array(T (&&a)[N]) {
 	return detail::to_array_impl(std::move(a), std::make_index_sequence<N> {});
 }
 
-#endif
+} // namespace std
+
+#endif //__cpp_lib_to_array
 
 #ifndef __cpp_lib_to_underlying
 	#define __cpp_lib_to_underlying 202102L
+
+namespace std {
 
 /// Convert an object of enumeration type to its underlying type.
 template <typename _Tp>
 constexpr underlying_type_t<_Tp> to_underlying(_Tp __value) noexcept {
 	return static_cast<underlying_type_t<_Tp>>(__value);
 }
-#endif // C++23
 
 } // namespace std
+
+#endif // __cpp_lib_to_underlying
