ee4d5c49ff54f991f63f126f04863b4a6b382de7
laszloh <laszlo.hegedues@gmail.com>
2023-12-05 14:01:19 +0100

    Add std::allocator which preferes PSRAM
    Adda custom allocators which will use the PSRAM of the ESP32 first, when allocating a string. With this and the usage of std::string we can use the default destructor to destory the Playlist std::vector.
    
    TODO: check if default allocator (with malloc) does not already use the PSRAM (with regards to CONFIG_SPIRAM_USE_MALLOC)

--- src/Playlist.h
+++ src/Playlist.h
@@ -3,16 +3,32 @@
 #include <stdlib.h>
 #include <vector>
 
-using Playlist = std::vector<char *>;
+template <typename T>
+class PsramAllocator {
+public:
+	using value_type = T;
+	using propagate_on_container_move_assignment = std::true_type;
+	using is_always_equal = std::true_type;
 
-// Release previously allocated memory
-inline void freePlaylist(Playlist *playlist) {
-	if (playlist == nullptr) {
-		return;
+	PsramAllocator() noexcept = default;
+	PsramAllocator(PsramAllocator const &) noexcept = default;
+	~PsramAllocator() = default;
+
+	template <typename U>
+	constexpr PsramAllocator(const PsramAllocator<U> const &) noexcept { }
+
+	// memory allocation
+	[[nodiscard]] T *allocate(std::size_t cnt) {
+		return (T *) heap_caps_malloc_prefer(cnt * sizeof(T), 2, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
 	}
-	for (auto e : *playlist) {
-		free(e);
+
+	void deallocate(T *p, std::size_t cnt) {
+		free(p);
 	}
-	delete playlist;
-	playlist = nullptr;
-}
+
+	constexpr bool operator==(PsramAllocator const &a) { return true; }
+	constexpr bool operator!=(PsramAllocator const &a) { return false; }
+};
+
+using pstring = std::basic_string<char, std::char_traits<char>, PsramAllocator<char>>;
+using Playlist = std::vector<pstring>;
