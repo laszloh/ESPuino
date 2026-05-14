d25ebf0c08fa8007c5b43851978c0cad32feec3c
Laszlo Hegedüs <laszlo.hegedues@gmail.com>
2026-05-14 20:10:21 +0200

    Add PSRAM allocator for std::vector and std::string

--- src/Playlist.h
+++ src/Playlist.h
@@ -1,13 +1,36 @@
 #pragma once
 
+#include <Arduino.h>
+
 #include <stdlib.h>
-#include <vector>
 #include <string>
+#include <vector>
 
-using Playlist = std::vector<std::string>;
+template <typename T>
+struct PsramAllocator {
+	using value_type = T;
 
-// Release previously allocated memory
-inline void freePlaylist(Playlist *playlist) {
-	delete playlist;
-	playlist = nullptr;
-}
+	PsramAllocator() = default;
+
+	template <typename U>
+	constexpr PsramAllocator(const PsramAllocator<U> &) noexcept { }
+
+	T *allocate(std::size_t n) {
+		static const bool hasPsram = psramFound();
+		void *ptr = hasPsram ? ps_malloc(n * sizeof(T)) : malloc(n * sizeof(T));
+		return static_cast<T *>(ptr);
+	}
+
+	void deallocate(T *ptr, std::size_t) noexcept {
+		free(ptr);
+	}
+
+	template <typename U>
+	bool operator==(const PsramAllocator<U> &) const noexcept { return true; }
+
+	template <typename U>
+	bool operator!=(const PsramAllocator<U> &) const noexcept { return false; }
+};
+
+using PsramString = std::basic_string<char, std::char_traits<char>, PsramAllocator<char>>;
+using Playlist = std::vector<PsramString, PsramAllocator<PsramString>>;
